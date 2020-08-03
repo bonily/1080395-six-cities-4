4138c736b33d1559d69f7ede043d3b02c9b3d468
bonily <bonilya@gmail.com>
2020-08-03 22:18:01 +0300

    Еще правки

--- src/common.js
+++ src/common.js
@@ -13,10 +13,6 @@ export const extend = (a, b) => {
   return Object.assign({}, a, b);
 };
 
-export const getUnicValues = (values) => {
-  return values.filter((value, i) => values.indexOf(value) === i);
-};
-
 export const getOffersByCity = (city, offers) => {
   return offers[city];
 };
